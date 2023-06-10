edc84d7123870226856006e9a67629a7c952175e
Sarav Ramaswamy <sarav@yahooinc.com>
2023-06-10 16:09:29 -0700

    Initial commit

---
+++ app/src/main/java/com/example/hellooverlay/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.hellooverlay
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        checkOverlayPermission()
+        startService()
+    }
+
+    // check for permission again when user grants it from
+    // the device settings, and start the service
+    override fun onResume() {
+        super.onResume()
+        startService()
+    }
+
+    // method for starting the service
+    private fun startService() {
+        // check if the user has already granted
+        // the Draw over other apps permission
+        if (Settings.canDrawOverlays(this)) {
+            // start the service based on the android version
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(Intent(this, ForegroundService::class.java))
+            } else {
+                startService(Intent(this, ForegroundService::class.java))
+            }
+        }
+    }
+
+    // method to ask user to grant the Overlay permission
+    private fun checkOverlayPermission() {
+        if (!Settings.canDrawOverlays(this)) {
+            // send user to the device settings
+            val myIntent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
+            startActivity(myIntent)
+        }
+    }
+}
